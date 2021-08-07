0e0bf2efeea57a40783718f2d93da7e8a023b403
Crowdbotics <team@crowdbotics.com>
2021-08-07 22:16:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "jolly_darkness_29624Identifier",
+  url: "https://jolly_darkness_29624Identifier.botics.co",
+  api: "https://jolly_darkness_29624Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
